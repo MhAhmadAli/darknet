68f7745cf990e7afcbd0b0a00bce782e87440f9b
Mohammad Ahmad Ali <42268785+MhAhmadAli@users.noreply.github.com>
2024-11-13 17:04:48 +0000

    small changes

--- src/convolutional_layer.c
+++ src/convolutional_layer.c
@@ -306,7 +306,7 @@ void cudnn_convolutional_setup(layer *l, int cudnn_preference, size_t workspace_
             l->weightDesc,
             l->convDesc,
             l->dstTensorDesc,
-            (cudnnConvolutionFwdPreference_t)forward_algo,
+            (cudnnConvolutionFwdAlgoPerf_t)forward_algo,
             workspace_size_specify,
             &l->fw_algo));
     CHECK_CUDNN(cudnnGetConvolutionBackwardDataAlgorithm(cudnn_handle(),
